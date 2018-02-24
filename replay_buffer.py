ded5b1ae5df9a9571238e2e6db6448d2fa2cc56e
percepticat <36747846+percepticat@users.noreply.github.com>
2018-02-24 09:02:39 -0800

    Add files via upload

---
+++ replay_buffer.py
@@ -0,0 +1,44 @@
+"""Memory replay buffer for reinforcement learning."""
+
+import random
+
+from collections import namedtuple
+
+Experience = namedtuple("Experience",
+    field_names=["state", "action", "reward", "next_state", "done"])
+
+
+class ReplayBuffer:
+    """Fixed-size circular buffer to store experience tuples."""
+
+    def __init__(self, size=1000):
+        """Initialize a ReplayBuffer object."""
+        self.size = size  # maximum size of buffer
+        self.memory = []  # internal memory (list)
+        self.idx = 0  # current index into circular buffer
+    
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        # TODO: Create an Experience object, add it to memory
+        # Note: If memory is full, start overwriting from the beginning
+        if len(self.memory) < self.size:
+            self.memory.append(Experience._make(
+            [state, action, reward, next_state, done]))
+        else:
+            if (self.idx == self.size):
+                self.idx = 0
+            self.memory[self.idx] = Experience._make(
+            [state, action, reward, next_state, done])
+            self.idx += 1
+        
+    def sample(self, batch_size=64):
+        """Randomly sample a batch of experiences from memory."""
+        # TODO: Return a list or tuple of Experience objects sampled from memory
+        return random.sample(self.memory, k=batch_size)
+        
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+	
+
